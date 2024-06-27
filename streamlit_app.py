593f41c8ee6d235f8681bd53af2144af72a6ba4c
Jem Grisham <jemgrisham@gmail.com>
2024-06-27 15:13:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-import requests
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -29,10 +29,10 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+            ingredients_string += fruit_chosen + ' '
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+            fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
 
